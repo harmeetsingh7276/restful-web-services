6ea1fc15b8efbdea505991dd94a11394b0fd8741
harmeet <harmeetsingh7276@gmail.com>
2023-12-20 00:06:40 +0530

    added user model and getAll and getUserById method

---
+++ src/main/java/com/harmeet/rest/webservices/restfulwebservices/socialmedia/controller/UserResource.java
@@ -0,0 +1,28 @@
+package com.harmeet.rest.webservices.restfulwebservices.socialmedia.controller;
+
+import com.harmeet.rest.webservices.restfulwebservices.socialmedia.dao.UserDaoService;
+import com.harmeet.rest.webservices.restfulwebservices.socialmedia.user.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public List<User> retrieveUserById(@PathVariable Integer id){
+        return userDaoService.findByUserId(id);
+    }
+}
---
+++ src/main/java/com/harmeet/rest/webservices/restfulwebservices/socialmedia/dao/UserDaoService.java
@@ -0,0 +1,27 @@
+package com.harmeet.rest.webservices.restfulwebservices.socialmedia.dao;
+
+import com.harmeet.rest.webservices.restfulwebservices.socialmedia.user.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Harmeet", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Payal", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Renu", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public List<User> findByUserId(Integer id) {
+        return users.stream().filter(user -> user.getId()==id).toList();
+    }
+}
