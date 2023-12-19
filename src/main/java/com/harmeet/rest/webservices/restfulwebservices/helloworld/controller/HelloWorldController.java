cba472c2a516ffce319000a03a3dee4645b32259
harmeet <harmeetsingh7276@gmail.com>
2023-12-19 23:16:44 +0530

    returned a HelloWorldBean

--- src/main/java/com/harmeet/rest/webservices/restfulwebservices/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/harmeet/rest/webservices/restfulwebservices/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.harmeet.rest.webservices.restfulwebservices.helloworld.controller;
 
+import com.harmeet.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World 111112222221";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
