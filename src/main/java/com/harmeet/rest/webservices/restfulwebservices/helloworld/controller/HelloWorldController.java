65d13b32c84d93f237638d9b4739b7af450dfcc2
harmeet <harmeetsingh7276@gmail.com>
2023-12-20 02:12:54 +0530

    added i18n support

--- src/main/java/com/harmeet/rest/webservices/restfulwebservices/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/harmeet/rest/webservices/restfulwebservices/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,23 @@
 package com.harmeet.rest.webservices.restfulwebservices.helloworld.controller;
 
 import com.harmeet.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World 111112222221";
@@ -21,4 +32,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldi18n() {
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+    }
+
 }
