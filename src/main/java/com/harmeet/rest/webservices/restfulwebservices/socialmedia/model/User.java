7ecf48bebd7a2710471e89cb94d3de61766c337c
harmeet <harmeetsingh7276@gmail.com>
2023-12-20 20:25:04 +0530

    added post entity

--- src/main/java/com/harmeet/rest/webservices/restfulwebservices/socialmedia/model/User.java
+++ src/main/java/com/harmeet/rest/webservices/restfulwebservices/socialmedia/model/User.java
@@ -1,13 +1,17 @@
 package com.harmeet.rest.webservices.restfulwebservices.socialmedia.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
+
 @Entity(name="user_details")
 public class User {
     @Id
@@ -19,6 +23,9 @@ public class User {
     @Past(message = "Birth Date should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     @Override
     public String toString() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
 insert into user_details(id,birth_date,name) values (10001,CURRENT_DATE(),'Harmeet');
 insert into user_details(id,birth_date,name) values (10002,CURRENT_DATE(),'Payal');
 insert into user_details(id,birth_date,name) values (10003,CURRENT_DATE(),'Renu');
+
+insert into post(id,description,user_id) values (20001,'LEARN AWS',10001);
+insert into post(id,description,user_id) values (20002,'LEARN GCP',10001);
+insert into post(id,description,user_id) values (20003,'LEARN DEVOPS',10001);
+
+insert into post(id,description,user_id) values (20004,'CERTIFY DEVOPS',10002);
+
+insert into post(id,description,user_id) values (20005,'CERTIFY AWS',10002);
