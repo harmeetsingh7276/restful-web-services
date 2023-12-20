7e2ae390c48558e1c29e56b96b6d050b66937444
harmeet <harmeetsingh7276@gmail.com>
2023-12-20 20:05:25 +0530

    added a user entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id,birth_date,name) values (10001,CURRENT_DATE(),'Harmeet');
+insert into user_details(id,birth_date,name) values (10002,CURRENT_DATE(),'Payal');
+insert into user_details(id,birth_date,name) values (10003,CURRENT_DATE(),'Renu');
